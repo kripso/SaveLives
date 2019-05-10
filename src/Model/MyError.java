120b42b5b652a6ee216ab5edfcf45aee8bcff98b
Roland Rajcsanyi <kripsoworld@gmail.com>
2019-05-10 09:08:13 +0200

    Error changes

--- src/Model/MyError.java
+++ src/Model/MyError.java
@@ -6,14 +6,24 @@ import javafx.scene.control.Alert;
 
 public class MyError extends Exception {
     String username;
+    String missingInfo;
 
     public MyError(String login) {
         username = login;
     }
-    public void UsernameNotFoundAlert(String text) {
+    public MyError(int index) {
+        if (index == 0){this.missingInfo = "Missing Email";}
+        if (index == 1){this.missingInfo = "Missing Address";}
+        if (index == 2){this.missingInfo = "Missing Phone number";}
+    }
+
+    public void UsernameNotFoundAlert() {
         new UsernameNotFoundAlert(Alert.AlertType.INFORMATION,username);
     }
-    public void UserNotFoundAlert(String text) {
+    public void UserNotFoundAlert() {
         new UserNotFoundAlert(Alert.AlertType.INFORMATION,username);
+    }
+    public void MissingInfoAlert() {
+        new UserNotFoundAlert(Alert.AlertType.INFORMATION,missingInfo);
     }
 }
