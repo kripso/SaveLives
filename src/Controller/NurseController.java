7be0b2876838f3fd789c4952fbf737e1ea0efdef
Roland Rajcsanyi <kripsoworld@gmail.com>
2019-05-11 16:54:59 +0200

    Added searching Funcionality added set on action

--- src/Controller/NurseController.java
+++ src/Controller/NurseController.java
@@ -4,16 +4,14 @@ import Database.Data;
 import Model.DonorInfo;
 import Model.MyError;
 import Model.PersonalInfo;
-import View.UserNotFoundAlert;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import users.SaveDonorInfo;
 import users.SavePersonalInfo;
-
 import java.io.IOException;
 
-public class NurseController extends UserController {
+public abstract class NurseController extends UserController{
 
     public TextField Hospital;
 
@@ -28,6 +26,9 @@ public class NurseController extends UserController {
 
     public DonorInfo donorInfo = Data.getInstance().donorInfo;
 
+    public Button addPersonalDonorInfoButton;
+    public Button addPatientDonorInfoButton;
+
     public NurseController(){
 
     }
@@ -41,26 +42,52 @@ public class NurseController extends UserController {
             BoneMarrowHLA.setText(Data.getInstance().donorInfo.getBoneMarrow());
             Hospital.setText(Data.getInstance().personalInfo.getHospital());
         }
+        addPatientDonorInfoButton.setOnAction(event ->{
+            try {
+                addPatientDonorInfo();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+        addPersonalDonorInfoButton.setOnAction(event -> {
+            try {
+                addPersonalDonorInfo();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    public void addInformationPressed(ActionEvent actionEvent) throws IOException, ClassNotFoundException {
+    @Override
+    public void addInformationPressed() throws IOException, ClassNotFoundException {
         PersonalInfo personalInfo = new PersonalInfo(Data.getInstance().abstractUser.getUserName(),Name.getText(),Surname.getText(),Address.getText(),Email.getText(),PhoneNumber.getText(),Hospital.getText());
         SavePersonalInfo.personalInfo(personalInfo);
         Data.getInstance().personalInfo = personalInfo;
+        if(this instanceof DoctorController){
+            setUpTableView();
+        }
     }
 
-    public void addPersonalDonorInfo(ActionEvent actionEvent) throws IOException, ClassNotFoundException {
+    public void addPersonalDonorInfo() throws IOException, ClassNotFoundException {
         DonorInfo donorInfo = new DonorInfo(Data.getInstance().abstractUser.getUserName(),BloodType.getText(),PlasmaRHD.getText(),BoneMarrowHLA.getText());
         SaveDonorInfo.updateDonorInfo(donorInfo,Data.getInstance().abstractUser.getUserName());
         Data.getInstance().donorInfo = donorInfo;
+        if(this instanceof DoctorController){
+            setUpTableView();
+        }
     }
 
-    public void addPatienDonorInfo(ActionEvent actionEvent) throws IOException, ClassNotFoundException {
+    public void addPatientDonorInfo() throws IOException, ClassNotFoundException {
         try {
             DonorInfo donorInfo = new DonorInfo(UsernameOfPatient.getText(),AddBloodType.getText(),AddPlasmaRHD.getText(),AddBoneMarrowHLA.getText());
             SaveDonorInfo.addDonorInfo(donorInfo,UsernameOfPatient.getText());
+            if(this instanceof DoctorController){
+                setUpTableView();
+            }
         } catch (MyError myError) {
             myError.UserNotFoundAlert();
         }
     }
+
+    public abstract void setUpTableView() throws IOException, ClassNotFoundException;
 }
